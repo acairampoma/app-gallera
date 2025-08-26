d8029024c6016b5e69f07a098e34f442bcf5f34a
Alan Cairampoma <alancairampoma@gmail.com>
2025-08-26 00:12:08 -0500

    version de ios fluter 9

--- lib/main.dart
+++ lib/main.dart
@@ -101,7 +101,7 @@ class CastaDeGallosApp extends StatelessWidget {
             color: Colors.white,
           ),
         ),
-        cardTheme: CardTheme(
+        cardTheme: CardThemeData(
           elevation: 2,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(15),
